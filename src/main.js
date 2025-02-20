23f213954fe10dc94bc7be40e56bbedfa2f9a863
Sara <sarailic431@gmail.com>
2025-02-20 15:38:59 +0100

    working on js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,47 @@
 "use strict";
 import "./style.css";
+
+const inputTextEl = document.querySelector(".input-text");
+const inputNumberEl = document.querySelector(".input-number");
+const form = document.querySelector(".form");
+
+let inputText;
+let inputNum;
+
+inputTextEl.addEventListener("input", function (e) {
+  inputText = e.target.value;
+});
+
+inputNumberEl.addEventListener("input", function (e) {
+  inputNum = e.target.value;
+});
+
+class Expense {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.id = crypto.randomUUID();
+  }
+}
+
+class ExpenseManager {
+  constructor() {
+    this.expenses = [];
+  }
+
+  add(exp) {
+    this.toDos.push(exp);
+  }
+
+  filter(item) {
+    this.expenses = this.expenses.filter((exp) => exp.id !== item.id);
+  }
+}
+
+const manager = new ExpenseManager();
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const expense = new Expense();
+});

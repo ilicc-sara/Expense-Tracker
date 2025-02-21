3b30efa91846a0d326d171d79c51b5e3215b9413
Sara <sarailic431@gmail.com>
2025-02-21 17:22:59 +0100

    finished

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ const form = document.querySelector(".form");
 const itemContainer = document.querySelector(".items-container");
 const balanceNumberEl = document.querySelector(".balance-num");
 
+const incomeNumber = document.querySelector(".income-num");
+const expenseNumber = document.querySelector(".expense-num");
+
 let inputText;
 let inputNum;
 
@@ -18,7 +21,7 @@ inputNumberEl.addEventListener("input", function (e) {
   inputNum = e.target.value;
 });
 
-class Expense {
+class Transaction {
   constructor(name, price) {
     this.name = name;
     this.price = price;
@@ -26,28 +29,47 @@ class Expense {
   }
 }
 
-class ExpenseManager {
+class TransactionManager {
   constructor() {
-    this.expenses = [];
+    this.transactions = [];
     this.totalBalance = 0;
+
+    this.totalIncome = 0;
+    this.totalExpense = 0;
   }
 
   add(exp) {
-    this.expenses.push(exp);
+    this.transactions.push(exp);
   }
 
-  filter(item) {
-    this.expenses = this.expenses.filter((exp) => exp.id !== item.id);
+  filter(id) {
+    this.transactions = this.transactions.filter((exp) => exp.id !== id);
   }
 
   balance() {
-    return (this.totalBalance = this.expenses.reduce((acc, cur) => {
+    return (this.totalBalance = this.transactions.reduce((acc, cur) => {
       return acc + Number(cur.price);
     }, 0));
   }
+
+  positiveBalance() {
+    return (this.totalIncome = this.transactions
+      .filter((item) => Number(item.price) > 0)
+      .reduce((acc, cur) => {
+        return acc + Number(cur.price);
+      }, 0));
+  }
+
+  negativeBalance() {
+    return (this.totalIncome = this.transactions
+      .filter((item) => Number(item.price) < 0)
+      .reduce((acc, cur) => {
+        return acc - Number(cur.price);
+      }, 0));
+  }
 }
 
-const manager = new ExpenseManager();
+const manager = new TransactionManager();
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -55,11 +77,11 @@ form.addEventListener("submit", function (e) {
   if (inputNum === "0" || inputNum === undefined) {
     alert("Expense numbers should be real numbers only!");
   } else {
-    const expense = new Expense(inputText, inputNum);
+    const expense = new Transaction(inputText, inputNum);
     manager.add(expense);
 
     const item = document.createElement("li");
-    item.innerHTML = `<span>${inputText}</span> <span>$ ${inputNum}</span>`;
+    item.innerHTML = `<button class="delete">X</button><span>${inputText}</span> <span>$ ${inputNum}</span>`;
     item.setAttribute("data-id", expense.id);
     itemContainer.appendChild(item).className = "item";
 
@@ -69,6 +91,20 @@ form.addEventListener("submit", function (e) {
   }
 
   balanceNumberEl.textContent = manager.balance();
+  incomeNumber.textContent = `$ ${manager.positiveBalance()}`;
+  expenseNumber.textContent = `$ ${manager.negativeBalance()}`;
+
   inputNumberEl.value = "";
   inputTextEl.value = "";
+});
+
+itemContainer.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("delete")) return;
+
+  const targetId = e.target.closest(".item").getAttribute("data-id");
+  const targetEl = e.target.closest(".item");
+
+  manager.filter(targetId);
+
+  targetEl.remove();
 });
